fb6f9a79f3f2fb92ab801ae85c92f57baa2b2607
Kevin0811 <42699227+Kevin0811@users.noreply.github.com>
2021-04-05 21:04:38 +0800

    Add FSP

--- examples/exampleTransformDepthToColorOpenCV_blurBackground.py
+++ examples/exampleTransformDepthToColorOpenCV_blurBackground.py
@@ -4,6 +4,7 @@ sys.path.insert(0, '../pyKinectAzure/')
 import numpy as np
 from pyKinectAzure import pyKinectAzure, _k4a, postProcessing
 import cv2
+import time
 
 # Path to the module
 # TODO: Modify with the path containing the k4a.dll from the Azure Kinect SDK
@@ -19,99 +20,117 @@ def alphaBlend(img1, img2, mask):
         alpha = mask/255.0
     else:
         alpha = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)/255.0
+
+    # 此處僅使用convertScaleAbs函數輸出為unit8的功能
+    # 等同於(img1*(1-alpha) + img2*alpha).astype('uint8')
+    # 若非uint8格式，將無法正常顯示畫面
     blended = cv2.convertScaleAbs(img1*(1-alpha) + img2*alpha)
     return blended
 
 if __name__ == "__main__":
 
-	dis_near = 200
-	dis_far = 900
+    dis_near = 200
+    dis_far = 900
 
-	# Initialize the library with the path containing the module
-	pyK4A = pyKinectAzure(modulePath)
+    # Initialize the library with the path containing the module
+    pyK4A = pyKinectAzure(modulePath)
 
-	# Open device
-	pyK4A.device_open()
+    # Open device
+    pyK4A.device_open()
 
-	# Modify camera configuration
-	device_config = pyK4A.config
-	device_config.color_format = _k4a.K4A_IMAGE_FORMAT_COLOR_BGRA32
-	device_config.color_resolution = _k4a.K4A_COLOR_RESOLUTION_720P
-	device_config.depth_mode = _k4a.K4A_DEPTH_MODE_WFOV_2X2BINNED
-	print(device_config)
+    # Modify camera configuration
+    device_config = pyK4A.config
+    device_config.color_format = _k4a.K4A_IMAGE_FORMAT_COLOR_BGRA32
+    device_config.color_resolution = _k4a.K4A_COLOR_RESOLUTION_720P
+    device_config.depth_mode = _k4a.K4A_DEPTH_MODE_WFOV_2X2BINNED
+    print(device_config)
 
-	# Start cameras using modified configuration
-	pyK4A.device_start_cameras(device_config)
+    # Start cameras using modified configuration
+    pyK4A.device_start_cameras(device_config)
 
-	front_ground = np.zeros((1280,720))
-	front_ground_mask = np.zeros((1280,720))
-	back_ground = np.zeros((1280,720))
-	back_ground_mask = np.zeros((1280,720))
+    front_ground = np.zeros((1280,720))
+    front_ground_mask = np.zeros((1280,720))
+    back_ground = np.zeros((1280,720))
+    back_ground_mask = np.zeros((1280,720))
 
-	k = 0
+    k = 0
+    time_start = time.time()
+    cv2.namedWindow('Output Image',cv2.WINDOW_NORMAL)
 
-	while True:
-		# Get capture
-		pyK4A.device_get_capture()
+    while True:
+        # Get capture
+        pyK4A.device_get_capture()
 
-		# Get the depth image from the capture
-		depth_image_handle = pyK4A.capture_get_depth_image()
+        # Get the depth image from the capture
+        depth_image_handle = pyK4A.capture_get_depth_image()
 
-		# Get the color image from the capture
-		color_image_handle = pyK4A.capture_get_color_image()
+        # Get the color image from the capture
+        color_image_handle = pyK4A.capture_get_color_image()
 
-		# Check the image has been read correctly
-		if depth_image_handle and color_image_handle:
+        
 
-			# Read and convert the image data to numpy array:
-			color_image = pyK4A.image_convert_to_numpy(color_image_handle)[:,:,:3]
+        # Check the image has been read correctly
+        if depth_image_handle and color_image_handle:
 
-			front_ground = color_image.copy()
-			back_ground = color_image.copy()
+            # Read and convert the image data to numpy array:
+            color_image = pyK4A.image_convert_to_numpy(color_image_handle)[:,:,:3]
 
-			# Transform the depth image to the color format
-			transformed_depth_image = pyK4A.transform_depth_to_color(depth_image_handle,color_image_handle)
+            front_ground = color_image.copy()
+            back_ground = color_image.copy()
 
-			#maximum_hole_size = 10
-			#smoothed_depth_image = postProcessing.smooth_depth_image(transformed_depth_image, maximum_hole_size)
+            # Transform the depth image to the color format
+            transformed_depth_image = pyK4A.transform_depth_to_color(depth_image_handle,color_image_handle)
 
-			#height, width = color_image.shape[:2]
+            maximum_hole_size = 10
+            smoothed_depth_image = postProcessing.smooth_depth_image(transformed_depth_image, maximum_hole_size)
+            #print(smoothed_depth_image.shape)
 
-			mask = transformed_depth_image > dis_near
-			front_ground[mask] = 0
-			back_ground[~mask] = 0
-			#print(back_ground.dtype)
-			#front_ground_mask = 1*mask
-			back_ground_mask = (255*mask).astype('uint8')
-			#print(back_ground_mask.dtype)
+            #height, width = color_image.shape[:2]
 
-			back_ground = cv2.GaussianBlur(color_image, (21,21),11 )
-			back_ground_mask = cv2.GaussianBlur(back_ground_mask, (21,21),11 )
+            mask = smoothed_depth_image > dis_near
+            #print(mask.shape)
+            front_ground[mask] = 0
 
-			output_image = alphaBlend(color_image, back_ground, back_ground_mask)
-			#output_image = front_ground+back_ground
+            back_ground[~mask] = 0
+            #print(back_ground.dtype)
+            #front_ground_mask = 1*mask
+            back_ground_mask = (255*mask).astype('uint8')
+            #print(back_ground_mask.dtype)
+
+            back_ground = cv2.GaussianBlur(color_image, (21,21),11 )
+            back_ground_mask = cv2.GaussianBlur(back_ground_mask, (21,21),11 )
+
+            output_image = alphaBlend(color_image, back_ground, back_ground_mask)
+            #output_image = front_ground+back_ground
 
 
-			# Convert depth image (mm) to color, the range needs to be reduced down to the range (0,255)
-			#transformed_depth_color_image = cv2.applyColorMap(np.round(transformed_depth_image/30).astype(np.uint8), cv2.COLORMAP_JET)
+            # Convert depth image (mm) to color, the range needs to be reduced down to the range (0,255)
+            #transformed_depth_color_image = cv2.applyColorMap(np.round(transformed_depth_image/30).astype(np.uint8), cv2.COLORMAP_JET)
 
-			# Add the depth image over the color image:
-			#combined_image = cv2.addWeighted(color_image,0.7,transformed_depth_color_image,0.3,0)
-			
-			# Plot the image
-			cv2.namedWindow('Colorized Depth Image',cv2.WINDOW_NORMAL)
-			cv2.imshow('Output Image',output_image)
-			cv2.imshow('Depth Mask',back_ground_mask)
-			k = cv2.waitKey(25)
+            # Add the depth image over the color image:
+            #combined_image = cv2.addWeighted(color_image,0.7,transformed_depth_color_image,0.3,0)
 
-			pyK4A.image_release(depth_image_handle)
-			pyK4A.image_release(color_image_handle)
+            fps = int(60/(time.time()-time_start))
 
-		pyK4A.capture_release()
+            cv2.putText(output_image, "FPS: "+str(fps),(50,60), cv2.FONT_HERSHEY_SIMPLEX, 1, ( 0, 0, 255), 1, cv2.LINE_AA)
+            
+            # Plot the image
+            
+            cv2.imshow('Output Image',output_image)
 
-		if k==27:    # Esc key to stop
-			cv2.destroyAllWindows()
-			break
+            time_start = time.time()
+            #cv2.imshow('Depth Mask',back_ground_mask)
+            k = cv2.waitKey(10)
 
-	pyK4A.device_stop_cameras()
-	pyK4A.device_close()
+            pyK4A.image_release(depth_image_handle)
+            pyK4A.image_release(color_image_handle)
+
+        pyK4A.capture_release()
+
+        # Esc key to stop
+        if k==27:
+            cv2.destroyAllWindows()
+            break
+
+    pyK4A.device_stop_cameras()
+    pyK4A.device_close()
