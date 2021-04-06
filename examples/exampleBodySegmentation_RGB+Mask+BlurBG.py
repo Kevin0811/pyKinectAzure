f7b7d74e342e18c233d35c77f50e3f26ecc1b60c
Kevin0811 <42699227+Kevin0811@users.noreply.github.com>
2021-04-07 01:48:09 +0800

    apply OpenCL

--- examples/exampleBodySegmentation_RGB+Mask+BlurBG.py
+++ examples/exampleBodySegmentation_RGB+Mask+BlurBG.py
@@ -8,35 +8,14 @@ from pyKinectAzure import pyKinectAzure, _k4a
 #from kinectBodyTracker import kinectBodyTracker, _k4abt
 import cv2
 
+cv2.setNumThreads(10)
+
 # Path to the module
 # TODO: Modify with the path containing the k4a.dll from the Azure Kinect SDK
 modulePath = 'C:\\Program Files\\Azure Kinect SDK v1.4.1\\sdk\\windows-desktop\\amd64\\release\\bin\\k4a.dll' 
 bodyTrackingModulePath = 'C:\\Program Files\\Azure Kinect Body Tracking SDK\\sdk\\windows-desktop\\amd64\\release\\bin\\k4abt.dll'
 # under x86_64 linux please use r'/usr/lib/x86_64-linux-gnu/libk4a.so'
 # In Jetson please use r'/usr/lib/aarch64-linux-gnu/libk4a.so'
-
-def copyto(scr_Image, mask):
-    height = scr_Image.shape[0]
-    width = scr_Image.shape[1]
-    # 生成和原图一样高度和宽度的矩形（全为0）
-    dst_Image = scr_Image
-
-    # 以下是copyTo的算法原理：
-    # 先遍历每行每列（如果不是灰度图还需遍历通道，可以事先把mask图转为灰度图）
-    for row in range(mask.shape[0]):
-        for col in range(mask.shape[1]):
-
-            # 如果掩图的像素不等于0，则dst(x,y) = scr(x,y)
-            if mask[row, col] != 0:
-                # dst_image和scr_Image一定要高宽通道数都相同，否则会报错
-                for channel in range(3):
-                    dst_Image[mask] = 0
-                    
-            # 如果掩图的像素等于0，则dst(x,y) = 0
-            elif mask[row, col] == 0:
-                for channel in range(3):
-                    dst_Image[row, col, channel] = scr_Image[row, col, channel]
-    return dst_Image
 
 if __name__ == "__main__":
 
@@ -49,7 +28,7 @@ if __name__ == "__main__":
     # Modify camera configuration
     device_config = pyK4A.config
     #device_config.color_format = _k4a.K4A_IMAGE_FORMAT_COLOR_BGRA32
-    device_config.color_resolution = _k4a.K4A_COLOR_RESOLUTION_720P
+    device_config.color_resolution = _k4a.K4A_COLOR_RESOLUTION_1080P
     device_config.depth_mode = _k4a.K4A_DEPTH_MODE_NFOV_UNBINNED
     print(device_config)
 
@@ -84,6 +63,8 @@ if __name__ == "__main__":
             # Read and convert the RGB image data to numpy array:
             color_image = pyK4A.image_convert_to_numpy(color_image_handle)
 
+            color_image_u = cv2.UMat(color_image)
+
             # cv2.convertScaleAbs Depth to uint8 (255)
             # alpha is fitted by visual comparison with Azure k4aviewer results 
             depth_color_image = cv2.convertScaleAbs (depth_image, alpha=0.05)
@@ -106,21 +87,25 @@ if __name__ == "__main__":
             # 創建 Boolean 遮罩
             mask = transformed_custom_image > 0
 
+            mask_u = cv2.UMat((255*mask).astype('uint8'))
+
             #body_image_color_cv = cv2.fromarray(body_image_color)
 
             # reference https://stackoverflow.com/questions/42594993/gradient-mask-blending-in-opencv-python
 
             # 拷貝 原圖(color_image) 作為背景圖片
-            background = color_image.copy()
+            #background = color_image.copy()
             # 對背景圖執行遮罩
-            background[mask] = 0
+            #background[mask] = 0
 
             # 將原圖進行模糊處理
-            blur_image = cv2.GaussianBlur(color_image, (15,15),cv2.BORDER_DEFAULT)
+            blur_image_u = cv2.GaussianBlur(color_image_u, (15,15),cv2.BORDER_DEFAULT)
+            blur_image = cv2.UMat.get(blur_image_u)
             # 將遮罩圖進行模糊處理
-            blur_mask = cv2.GaussianBlur((255*mask).astype('uint8'), (15,15),cv2.BORDER_DEFAULT)
+            blur_mask_u = cv2.GaussianBlur(mask_u, (15,15),cv2.BORDER_DEFAULT)
             # 將遮罩圖轉為三通道
-            alpha = cv2.cvtColor(blur_mask, cv2.COLOR_GRAY2BGR)/255.0
+            alpha_u = cv2.cvtColor(blur_mask_u, cv2.COLOR_GRAY2BGR)
+            alpha = cv2.UMat.get(alpha_u)/255.0
             # 將原圖(前景)與模糊圖(背景)整合
             dst_image=(color_image*(1-alpha) + blur_image*alpha).astype('uint8')
 
@@ -128,7 +113,7 @@ if __name__ == "__main__":
             
             #cv2.imshow('Segmented Depth Image',combined_image)
             cv2.imshow('Output Image',dst_image)
-            cv2.imshow('Alpha Mask',blur_mask)
+            #cv2.imshow('Alpha Mask',blur_mask)
             
             k = cv2.waitKey(1)
 
